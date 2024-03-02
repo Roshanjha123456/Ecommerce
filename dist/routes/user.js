f8087e162e86836092e3b6dcad70993315b180d3
roshan <jha8447632759@gmail.com>
2024-03-02 23:33:34 +0530

    fixed errors

--- dist/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { newUser, getAllUsers, getUser, deleteUser } from "../controllers/user.js";
-import { adminOnly } from "../middleware/auth/adminOnly.js";
-const router = express.Router();
-router.post("/newUser", newUser);
-router.get("/getAllUsers", adminOnly, getAllUsers);
-router.get("/getUser/:id", adminOnly, getUser);
-router.delete("/deleteUser/:id", deleteUser);
-export default router;
